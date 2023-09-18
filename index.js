09e58e0b2e7918e7d3798be0d8ca96c3c24d6c79
Scott Gowell <sgowell@users.noreply.github.com>
2023-09-18 10:48:46 -0400

    Adding Interval, Adding Timestamp, Message silenced count = 0

--- index.js
+++ index.js
@@ -11,62 +11,64 @@ const { Twilio } = pkg;
 const clientId = process.env.TWITCH_CLIENT_ID;
 const clientSecret = process.env.TWITCH_CLIENT_SECRET;
 
+const refreshDuration = 60 * 1000 * 10 * .5 ; //5 minutes
+
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = new Twilio(accountSid, authToken);
 
 const authProvider = new AppTokenAuthProvider(clientId, clientSecret);
-const barWithEndline = '---\n';
 
 const apiClient = new ApiClient({ authProvider });
-await apiClient.games
-  .getGamesByIgdbIds([process.env.IGDB_ID])
-  .then(async (games) => {
-    games.forEach(async (item) => {
-      await item.getStreams().then(async (gameStreams) => {
-        if (gameStreams.data.length < 1) {
-          const noStreams = `No ${item.name} Streams found\nCheck Back Later\nCheers.`;
-          console.log(noStreams);
-          sendMessage(noStreams);
-        } else {
-          gameStreams.data.forEach(async (stream) => {
-            var body = '';
-            const user = await stream.getUser();
+setInterval(async () => {
+  await apiClient.games
+    .getGamesByIgdbIds([process.env.IGDB_ID])
+    .then(async (games) => {
+      games.forEach(async (item) => {
+        await item.getStreams().then(async (gameStreams) => {
+          console.log(`\n${new Date(Date.now())}\n`);
+          if (gameStreams.data.length < 1) {
+            const noStreams = `No ${item.name} Streams found\nCheck Back Later\nCheers.`;
+            console.log(noStreams);
+            // sendMessage(noStreams);
+          } else {
+            gameStreams.data.forEach(async (stream) => {
+              var body = '';
+              const user = await stream.getUser();
 
-            apiClient.chat.getSettings(user.id).then(async (settings) => {
-              const twitchUrl = `Twitch URL: twitch://open?stream=${user.name}\n`;
-              body += twitchUrl;
-              console.log(twitchUrl);
+              apiClient.chat.getSettings(user.id).then(async (settings) => {
+                const twitchUrl = `Twitch URL: twitch://open?stream=${user.name}\n`;
+                body += twitchUrl;
+                console.log(twitchUrl);
 
-              if (stream.language !== 'en') { 
-                const language = `language: ${stream.language}\n`;
+                if (stream.language !== 'en') { 
+                  const language = `language: ${stream.language}\n`;
 
-                var translatedMessage = await buildTranslatedMessage(stream.language);
+                  var translatedMessage = await buildTranslatedMessage(stream.language);
 
-                body += language;
-                body += `${translatedMessage}\n`;
-                body += barWithEndline;
+                  body += language;
+                  body += `${translatedMessage}\n`;
                 
-                console.log(language);
-              }
+                  console.log(language);
+                }
 
-              const followerModeEnabled = settings.followerOnlyModeEnabled;
-              if (followerModeEnabled) {
-                const delay = settings.followerOnlyModeDelay ?? 'none';
-                const followerDelay = `Followers Only - Delay: ${delay} minutes\n`;
-                body += followerDelay;
-                console.log(followerDelay);
-              }
-              
-              sendMessage(body);
-              console.log(body);
-              console.log(barWithEndline);
+                const followerModeEnabled = settings.followerOnlyModeEnabled;
+                if (followerModeEnabled) {
+                  const delay = settings.followerOnlyModeDelay ?? 'none';
+                  const followerDelay = `Followers Only - Delay: ${delay} minutes\n`;
+                  body += followerDelay;
+                  console.log(followerDelay);
+                }
+                body += `\n${new Date(Date.now())}\n`;
+                sendMessage(body);
+                console.log(body);
+              });
             });
-          });
-        }
+          }
+        });
       });
     });
-  });
+}, refreshDuration);
 
 function sendMessage(body) {
   client.messages
